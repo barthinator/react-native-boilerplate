09b74c2b1e0ffdde69120b1a977fab131186007d
David B <eldor@Davids-iMac.local>
2018-01-16 22:06:59 -0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="dvvy" />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
